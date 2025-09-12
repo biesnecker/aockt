3c820fa598e52eec25444242b084469cef0e515d
John Biesnecker <jbiesnecker@gmail.com>
2025-09-12 06:06:37 -0400

    202409-1

---
+++ app/src/main/kotlin/io/jwb/aoc/year2024/Day09.kt
@@ -0,0 +1,31 @@
+package io.jwb.aoc.year2024
+
+import io.jwb.aoc.utils.getInputAsSingleString
+
+
+class Day09(input: String) {
+    val disk = input
+        .trim()
+        .windowed(2, 2, true)
+        .withIndex()
+        .flatMap { (index, value) -> List(value.first().digitToInt()) { index.toLong() } +
+                List(value.getOrElse(1) { '0' }.digitToInt()) { null }
+    }
+
+    fun partOne(): Long {
+        val empties = disk.indices.filter { disk[it] == null }.toMutableList()
+        return disk.withIndex().reversed().sumOf { (index, value) ->
+            if (value != null) {
+                value * (empties.removeFirstOrNull() ?: index)
+            } else {
+                empties.removeLastOrNull()
+                0
+            }
+        }
+    }
+}
+
+fun main() {
+    val solution = Day09(getInputAsSingleString(2024, 9))
+    println("Part 1: ${solution.partOne()}")
+}
